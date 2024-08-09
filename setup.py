348654836e0f84cbf37d2769238d7e83ca7a96b2
ankitgaur0 <ankitparashar000@gmail.com>
2024-08-09 13:26:52 +0530

    added setup file and Requirements.txt file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str) ->List[str] :
+    '''
+         this function iterate the packages from the Requirements.txxt file
+         and return in the setup function to execute .
+    '''
+    Requirements=[]
+    with open(file_path) as file_obj:
+        Requirements=file_obj.readlines()
+        #use list compresension for replace the \n to blank space ""
+        Requirements=[req.replace("\n", "") for req in Requirements]
+
+        if HYPEN_E_DOT in Requirements:
+            Requirements.remove(HYPEN_E_DOT)
+
+    return Requirements
+
+
+
+setup(
+    name="coffee_prediction",
+    version='0.0.01',
+    author="ankit1",
+    author_email="ankitparashar000@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('Requirements.txt')
+)
